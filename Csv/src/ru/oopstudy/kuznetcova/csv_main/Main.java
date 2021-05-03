892faf9eee3afea559bcf91becce5eb66709988b
nyany <nyanya89@mail.ru>
2021-05-03 17:33:53 +0700

    csv

--- Csv/src/ru/oopstudy/kuznetcova/csv_main/Main.java
+++ Csv/src/ru/oopstudy/kuznetcova/csv_main/Main.java
@@ -6,34 +6,94 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(new FileInputStream("file.csv"));
-             PrintWriter writer = new PrintWriter("result.html")) {
-            StringBuilder stringBuilder = new StringBuilder();
+        if (args.length != 2) {
+            System.out.println("В программу необходимо передать два аргумента: путь к файлу-источнику и путь к файлу для записи");
+            return;
+        }
 
-            stringBuilder.append("<!DOCTYPE html>");
-            stringBuilder.append("<html>");
-            stringBuilder.append("<head>");
-            stringBuilder.append("<meta charset=\"utf-8\">");
-            stringBuilder.append("</head>");
-            stringBuilder.append("<table border=\"1\">");
+        try (Scanner scanner = new Scanner(args[0]);
+             PrintWriter writer = new PrintWriter(args[1])) {
+            writer.println("<!DOCTYPE html>");
+            writer.println("<html>");
+            writer.println("<head>");
+            writer.println("<meta charset=\"utf-8\">");
+            writer.println("<title>Перевод CSV в HTML</title>");
+            writer.println("</head>");
+            writer.println("<body>");
+            writer.println("<table border=\"1\">");
+            writer.print("<tr><td>");
 
-            while (scanner.hasNext()) {
-                stringBuilder.append("<tr>");
-                String list = scanner.nextLine();
-                String[] splitList = list.split(",");
+            boolean isTextInTableDetail = false;
+            boolean isNewRow = false;
 
-                for (String s : splitList) {
-                    stringBuilder.append("<td>");
-                    stringBuilder.append(s);
-                    stringBuilder.append("</td>");
+            while (scanner.hasNextLine()) {
+                String s = scanner.nextLine();
+
+                if (s.isEmpty()) {
+                    continue;
                 }
 
-                stringBuilder.append("</tr>");
+                if (isNewRow && !isTextInTableDetail) {
+                    writer.print("<tr><td>");
+                }
+
+                char quotes = '"';
+                char comma = ',';
+                int i = 0;
+
+                if (s.charAt(0) == quotes) {
+                    isTextInTableDetail = true;
+                    i = 1;
+                }
+
+                for (; i < s.length(); i++) {
+                    char c = s.charAt(i);
+
+                    if (i == 0 && s.charAt(0) == comma) {
+                        writer.print("</td> <td>");
+                        isTextInTableDetail = false;
+                        continue;
+                    } else if (c == quotes && s.charAt(i - 1) == comma) {
+                        isTextInTableDetail = true;
+                        continue;
+                    } else if (c == comma && s.charAt(i - 1) == quotes) {
+                        isTextInTableDetail = false;
+                    } else if (c == quotes && isTextInTableDetail) {
+                        isTextInTableDetail = false;
+                        continue;
+                    } else if (c == quotes && s.charAt(i - 1) == quotes) {
+                        isTextInTableDetail = true;
+                    }
+
+                    if (c == '<') {
+                        writer.print("&lt;");
+                    } else if (c == '>') {
+                        writer.print("&gt;");
+                    } else if (c == '&') {
+                        writer.print("&amp;");
+                    } else if (c == comma && s.charAt(i - 1) == quotes
+                            && s.charAt(i - 2) == quotes
+                            && s.charAt(i - 3) != quotes) {
+                        writer.print(c);
+                    } else if (c == comma) {
+                        writer.print("</td> <td>");
+                    } else {
+                        writer.print(c);
+                    }
+                }
+
+                if (isTextInTableDetail) {
+                    writer.print("<br/>");
+                } else {
+                    writer.print("</td></tr>");
+                    isNewRow = true;
+                }
             }
 
-            stringBuilder.append("</table>");
-            stringBuilder.append("</html>");
-            writer.println(stringBuilder);
+            writer.println("");
+            writer.println("</table>");
+            writer.println("</body>");
+            writer.println("</html>");
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден");
         }
